3b385a300e61c0bea9fc527aaac0ff087cd9a7e8
Sitkov Illia <illiasitkov@gmail.com>
2022-02-20 19:33:47 +0200

    reload fix

--- server.js
+++ server.js
@@ -8,5 +8,7 @@ app.get('/', (req,res) => {
   res.sendFile(path.join(__dirname+'/dist/games-app/index.html'));
 });
 
+app.get('/*', (req, res) => res.send(__dirname+'/dist/games-app/index.html'));
+
 app.listen(process.env.PORT || 8080);
 
