47eedb5565c6cc6f6683321a1f82b12810a0f419
Sitkov Illia <illiasitkov@gmail.com>
2022-02-20 19:25:03 +0200

    firebase config for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBdwcXQidyl9HuXWswdy23i_-Q13mvDEAI",
+    authDomain: "games-app-1392a.firebaseapp.com",
+    projectId: "games-app-1392a",
+    storageBucket: "games-app-1392a.appspot.com",
+    messagingSenderId: "374782882032",
+    appId: "1:374782882032:web:baed97b278ddef5a2d5235",
+    measurementId: "G-GEFKYEX28X"
+  }
 };
