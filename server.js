894c6f4aa1f06a6b2cf33c55d613d31438aa9659
Sitkov Illia <illiasitkov@gmail.com>
2022-02-20 19:44:34 +0200

    reload fix

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 app.use(express.static(__dirname+'/dist/games-app'));
 
-app.get('/', (req,res) => {
-  res.sendFile(path.join(__dirname+'/dist/games-app/index.html'));
+app.get('*', (req,res) => {
+  res.sendFile(path.join(__dirname,'/dist/games-app/index.html'));
 });
 
-app.get('/*', (req, res) => res.send(__dirname+'/dist/games-app/index.html'));
+// app.get('/*', (req, res) => res.send(__dirname+'/dist/games-app/index.html'));
 
 app.listen(process.env.PORT || 8080);
 
